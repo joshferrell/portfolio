2d4ddc2f1e3f4e60c9a51f8935b971e6d6ec9144
Joshua Ferrell <jferrell@wha-mbp5.local>
2017-02-06 10:14:19 -0800

    feat: created hello world setup for application

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: './dist',
+        filename: 'index.js'
+    },
+    devServer: {
+        inline: true,
+        port: 8080
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015']
+                }
+            }
+        ]
+    }
+}
